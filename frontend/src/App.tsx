5fe35f75bb3c03af94819a60f0767d75e07799c4
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-09-26 15:25:43 -0700

    Cleanup and add new folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import { HomePage } from './pages/Home.page';
 import { theme } from './theme';
 
 export default function App() {
-  return (
-    <MantineProvider theme={theme}>
-      <HomePage />
-    </MantineProvider>
-  );
+  return <MantineProvider theme={theme}>App</MantineProvider>;
 }
