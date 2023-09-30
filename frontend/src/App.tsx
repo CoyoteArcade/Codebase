180d3280306292cf86a56b7e9a775b2b85bd4d17
servin415 <143432937+servin415@users.noreply.github.com>
2023-09-30 15:18:57 -0700

    Added Hero sectio and Navbar section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
+import { Flex } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { theme } from './theme';
 
 import Header from './sections/Header/Header';
+import Hero from './sections/Hero/Hero';
+import Navbar from './sections/Navbar/Navbar';
 
 export default function App() {
   return (
     <MantineProvider defaultColorScheme="light" theme={theme}>
       <Header />
+      <Flex >
+        <Navbar />
+        <Hero />
+      </Flex >
     </MantineProvider>
   );
 }
+
