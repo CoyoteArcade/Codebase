1398e48aa11471f048d50b414a39ef04b6d5813b
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-10-16 22:04:17 -0700

    Add category rows to 'Shop' section

--- frontend/src/sections/Shop/ShopGrid/ShopGrid.tsx
+++ frontend/src/sections/Shop/ShopGrid/ShopGrid.tsx
@@ -1,32 +1,20 @@
 import { Grid } from '@mantine/core';
 
-import GameCard from './GameCard/GameCard';
+import GameCard from '../GameCard/GameCard';
 
-import games from './GameCard/games.json';
+import classes from './ShopGrid.module.css';
 
-function getRandomNum(min = 300, max = 500) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin).toString(); // Inclusive both sides
-}
+import games from '../games.json';
 
 function GameGrid() {
   const gameList = games.map((game) => (
-    <Grid.Col span={{ base: 6, xs: 4, sm: 6, lg: 4, xl: 3, xxl: 2.4 }}>
-      <GameCard
-        gameObj={{
-          ...game,
-          banner: {
-            src: `https://placekitten.com/${getRandomNum()}/${getRandomNum()}`,
-            alt: 'kitto',
-          },
-        }}
-      />
+    <Grid.Col span={{ base: 6, xs: 4, sm: 4, md: 4, lg: 3, xl: 3, xxl: 2.4 }}>
+      <GameCard gameObj={game} />
     </Grid.Col>
   ));
 
   return (
-    <Grid gutter={{ base: 5, sm: 'md', lg: 'xl' }} justify="flex-start">
+    <Grid className={classes.grid} gutter={{ base: 5, sm: 'md', lg: 'xl' }} justify="flex-start">
       {gameList}
     </Grid>
   );
