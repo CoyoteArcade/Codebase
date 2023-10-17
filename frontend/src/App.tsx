c7598e74ae1c2aee25332f37422878026dcad2e4
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-10-16 22:14:03 -0700

    Add Footer section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,14 @@ import { theme } from './theme';
 
 import Header from './sections/Header/Header';
 import Shop from './sections/Shop/Shop';
+import Footer from './sections/Footer/Footer';
 
 export default function App() {
   return (
     <MantineProvider defaultColorScheme="light" theme={theme}>
       <Header />
       <Shop />
+      <Footer />
     </MantineProvider>
   );
 }
--- frontend/src/sections/Footer/Footer.tsx
+++ frontend/src/sections/Footer/Footer.tsx
@@ -1,7 +1,45 @@
+import { Anchor, Group, Container, ActionIcon, rem } from '@mantine/core';
+import { IconBrandGithub } from '@tabler/icons-react';
+
+import Logo from '../../components/Logo/Logo';
 import classes from './Footer.module.css';
 
+const links = [
+  { link: '#', label: 'Link 1' },
+  { link: '#', label: 'Link 2' },
+  { link: '#', label: 'Link 3' },
+  { link: '#', label: 'Link 4' },
+];
+
 function Footer() {
-  return <></>;
+  const items = links.map((link) => (
+    <Anchor
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      lh={1}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Logo />
+
+        <Group className={classes.links}>{items}</Group>
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandGithub style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  );
 }
 
 export default Footer;
