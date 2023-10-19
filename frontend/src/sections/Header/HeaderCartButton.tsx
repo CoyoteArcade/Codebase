c033d8af49b95d8f6c90e9d7fee1e7fb0135a3d2
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-10-19 03:05:26 -0700

    Add disabled styles for Header buttons

--- frontend/src/sections/Header/HeaderCartButton.tsx
+++ frontend/src/sections/Header/HeaderCartButton.tsx
@@ -3,25 +3,26 @@ import { IconShoppingCart } from '@tabler/icons-react';
 import classes from './Header.module.css';
 
 export default function CartButton() {
+  const cartDisabled = true;
+
   return (
-    <>
-      <Button
-        className={classes.button}
-        variant="transparent"
-        leftSection={
-          <Indicator
-            position="top-start"
-            label="0"
-            radius="xl"
-            size="16"
-            color="var(--mantine-color-teal-6)"
-          >
-            <IconShoppingCart size={25} />
-          </Indicator>
-        }
-      >
-        Cart
-      </Button>
-    </>
+    <Button
+      className={classes.button}
+      disabled={cartDisabled}
+      variant="transparent"
+      leftSection={
+        <Indicator
+          position="top-start"
+          label="0"
+          radius="xl"
+          size="16"
+          color={cartDisabled ? 'var(--mantine-color-teal-1)' : 'var(--mantine-color-teal-6'}
+        >
+          <IconShoppingCart size={25} />
+        </Indicator>
+      }
+    >
+      Cart
+    </Button>
   );
 }
