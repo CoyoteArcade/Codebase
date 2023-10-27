0b8bab1e9f9eb97759aa50069d1fedbf353c8f48
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-10-27 13:50:54 -0700

    Hide navbar from homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { Flex, Group } from '@mantine/core';
-import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import { theme } from './theme';
 
 import Header from './sections/Header/Header';
 import Hero from './sections/Hero/Hero';
-import Navbar from './sections/Navbar/Navbar';
+// import Navbar from './sections/Navbar/Navbar';
 import Shop from './sections/Shop/Shop';
 import Footer from './sections/Footer/Footer';
 
@@ -13,17 +12,10 @@ export default function App() {
   return (
     <MantineProvider defaultColorScheme="light" theme={theme}>
       <Header />
-      <Flex >
-        <Navbar />
-        <Group>
-          <Hero />
-          <Shop />
-          <Footer />
-        </Group>
-      </Flex >
-      
-      
+      {/* <Navbar /> */}
+      <Hero />
+      <Shop />
+      <Footer />
     </MantineProvider>
   );
 }
-
