b9042bf851c752d615cd4299a4946edda9a08ed8
Yair Torres <yairtorres844@gmail.com>
2023-12-02 20:40:33 -0800

    upload/download func

--- backend/test.js
+++ backend/test.js
@@ -1,10 +1,63 @@
-import { passwordReset } from "./index.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll, deleteObject } from "firebase/storage";
+import { listFiles, getFileUrl, uploadFile, deleteFile } from "/Users/Yair-/Documents/CSE4550/Coyote-Arcade/Codebase/backend/index.js";
+import { readFile } from "node:fs/promises";
+const storage = getStorage();
 
-const EMAIL = "tommy1012012938@gmail.com";
+// const uploadFile = async (file, path) => {
+//     if (!file) return;
+//     const storageRef = ref(storage, path);
+//     try {
+//         const snapshot = await uploadBytes(storageRef, file);
+//         console.log("Uploaded a blob or file!", snapshot);
+//         return snapshot;
+//     } catch (error) {
+//         console.error("Error uploading file", error);
+//         throw error;
+//     }
+// };
 
+// const getFileUrl = async (path) => {
+//     const storageRef = ref(storage, path);
+//     try {
+//         const url = await getDownloadURL(storageRef);
+//         return url;
+//     } catch (error) {
+//         console.error('Error getting file URL', error);
+//         throw error;
+//     }
+// };
+
+// const deleteFile = async (path) => {
+//     const storageRef = ref(storage, path);
+//     try {
+//         await deleteObject(storageRef);
+//         console.log('File deleted successfully');
+//     } catch (error) {
+//         console.error('Error deleting file', error);
+//         throw error;
+//     }
+// };
+
+// const listFiles = async (path) => {
+//     const listRef = ref(storage, path);
+//     try {
+//         const res = await listAll(listRef);
+//         return res.items; // Array of file references
+//     } catch (error) {
+//         console.error('Error listing files', error);
+//         throw error;
+//     }
+// };
+
+// const main = async () => {
+//     const path = "test/test.txt";
+//     const uploadedFile = await deleteFile(path);
+//     return uploadedFile;
+// };
 const main = async () => {
-    // await passwordReset(EMAIL);
-    // process.exit(0);
+    const path = "test.txt";
+    const deletedFile = await deleteFile(path);
+    return deletedFile;
 }
 
 main();
