078fa26441f0d859d24fa591a5c3eee1b5eec720
Chris Hyer <hyer.chris@gmail.com>
2023-12-03 22:43:12 -0800

    addded local session auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,19 @@ import '@mantine/tiptap/styles.css';
 import { Router } from './Router';
 import { theme } from './theme';
 import { AuthContext } from './utilities/auth/AuthContext';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './global.css';
 
 export default function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('coyoteArcadeUser');
+    if(storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{user, setUser} as any}>
