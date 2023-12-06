9a88b3a9a2394f319807151907078482af8dca76
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-12-06 13:58:17 -0800

    Upload: Add file validation for archive uploads

--- frontend/src/pages/Upload/PlatformsInput/FileUpload.tsx
+++ frontend/src/pages/Upload/PlatformsInput/FileUpload.tsx
@@ -1,15 +1,28 @@
 import { useState, useEffect, useRef } from 'react';
-import { Box, FileButton, Button, Group, Text, ThemeIcon } from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
+import { Box, FileButton, Code, Button, Group, Text, ThemeIcon } from '@mantine/core';
+import { IconCheck, IconX } from '@tabler/icons-react';
 
 const ARCHIVE_TYPES =
-  'application/gzip,application/zip,application/vnd.rar,application/x-7z-compressed';
+  'application/zip,application/x-7z-compressed,application/gzip,application/vnd.rar,application/x-zip-compressed,application/x-rar-compressed,application/x-rar';
 
 export default function FileUpload({ file, setFile }: { file: any; setFile: any }) {
   const resetRef = useRef<() => void>(null);
+  const [error, setError] = useState(false);
+
+  const validateFile = (file: any) => {
+    setError(false);
+
+    if (ARCHIVE_TYPES.split(',').includes(file.type)) {
+      setFile(file);
+    } else {
+      clearFile();
+      setError(true);
+    }
+  };
 
   const clearFile = () => {
     setFile(null);
+    setError(false);
     resetRef.current?.();
   };
 
@@ -17,8 +30,9 @@ export default function FileUpload({ file, setFile }: { file: any; setFile: any 
 
   return (
     <Box>
+      {/* Upload & Reset Buttons */}
       <Group justify="center">
-        <FileButton resetRef={resetRef} onChange={setFile} accept={ARCHIVE_TYPES}>
+        <FileButton resetRef={resetRef} onChange={validateFile} accept={ARCHIVE_TYPES}>
           {(props) => <Button {...props}>Upload Archive</Button>}
         </FileButton>
         <Button disabled={!file} color="red" onClick={clearFile}>
@@ -26,6 +40,7 @@ export default function FileUpload({ file, setFile }: { file: any; setFile: any 
         </Button>
       </Group>
 
+      {/* Successful File Message */}
       {file && (
         <Group justify="center" gap={10}>
           <ThemeIcon mt="sm" size="xs" radius="xl" color="green">
@@ -33,6 +48,23 @@ export default function FileUpload({ file, setFile }: { file: any; setFile: any 
           </ThemeIcon>
           <Text size="md" ta="center" mt="sm">
             Game Archive: {file.name}
+          </Text>
+        </Group>
+      )}
+
+      {/* Invalid File Message */}
+      {error && (
+        <Group justify="center" gap={10}>
+          <ThemeIcon mt="sm" size="xs" radius="xl" color="red">
+            <IconX style={{ width: '60%', height: '60%' }} />
+          </ThemeIcon>
+          {/* prettier-ignore */}
+          <Text size="md" ta="center" mt="sm">
+            Invalid file format! Try {' '}
+            <Code>.zip</Code>{' '}
+            <Code>.rar</Code>{' '}
+            <Code>.7z</Code>{' or '}
+            <Code>.gz</Code>{' files.'}
           </Text>
         </Group>
       )}
