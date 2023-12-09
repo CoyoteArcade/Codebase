8687f092b9ca354fa19b870be83360962d73c182
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2023-12-08 18:28:01 -0800

    Revert Games component to fix single-category games not showing

--- frontend/src/pages/Games.tsx
+++ frontend/src/pages/Games.tsx
@@ -1,12 +1,28 @@
-import { useRouteLoaderData } from 'react-router-dom';
 import { Grid } from '@mantine/core';
 
 import Navbar from '../sections/Navbar/Navbar';
 import Shop from '@/sections/Shop/Shop';
 
-export default function Categories({ gameCategory = '' }: any) {
-  const games: any = useRouteLoaderData('root');
+function translateCategory(category: string) {
+  switch (category) {
+    case 'rpg':
+      return 'RPG';
+    case 'action':
+      return 'Action';
+    case 'multiplayer':
+      return 'Multiplayer';
+    case 'strategy':
+      return 'Strategy';
+    case 'fps':
+      return 'FPS';
+    case 'adventure':
+      return 'Adventure';
+    default:
+      return 'ALL GAMES';
+  }
+}
 
+export default function Categories({ gameCategory = '' }: any) {
   return (
     <Grid gutter={0}>
       <Grid.Col span={{ base: 0, lg: 2 }}>
@@ -14,10 +30,9 @@ export default function Categories({ gameCategory = '' }: any) {
       </Grid.Col>
       <Grid.Col span={{ base: 12, xl: 10 }}>
         <Shop
-          titleGrid={gameCategory ? gameCategory : 'All Games'}
+          titleGrid={gameCategory ? translateCategory(gameCategory) : 'All Games'}
           showCategories={false}
-          gameCategory={gameCategory ? gameCategory : ''}
-          sortBy="releaseDate"
+          gameCategory={gameCategory ? translateCategory(gameCategory) : ''}
         />
       </Grid.Col>
     </Grid>
